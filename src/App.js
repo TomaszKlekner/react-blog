dcee18966fed88bae44b6c32227551e36362bf19
Tomasz Klekner <tklekner@objectivity.co.uk>
2023-03-14 15:51:39 +0100

    Create new post

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from './About';
 import Missing from './Missing';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -38,7 +39,21 @@ function App() {
   ]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
   let navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'dd MMMM, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/');
+  };
 
   const handleDelete = (id) => {
     const postsList = posts.filter((post) => post.id !== id);
@@ -52,7 +67,18 @@ function App() {
       <Nav search={search} setSearch={setSearch} />
       <Routes>
         <Route path='/' element={<Home posts={posts} />} />
-        <Route path='/post' element={<NewPost />} />
+        <Route
+          path='/post'
+          element={
+            <NewPost
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+              handleSubmit={handleSubmit}
+            />
+          }
+        />
         <Route
           path='/post/:id'
           element={<PostPage posts={posts} handleDelete={handleDelete} />}
